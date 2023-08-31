585e42918d9e5ff05ea1ac8c55a2bd49e1bf8334
Gabriel Siriani <gabrielsiriani19@gmail.com>
2023-08-31 08:58:21 -0300

    DAL

---
+++ BLL/ClienteCRUD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DAL;
+using ClienteDTO;
+
+namespace ClienteBLL
+{
+    internal class ClienteCRUD
+    {
+        Conexao objDAL = new Conexao();
+
+        string tabela = "tbl_cliente";
+
+        public void InserirCliente(ClienteCRUD objProdutoDTO)
+        {
+
+        }
+    }
+}
